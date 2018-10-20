2fdbc1a30261f24d73ecb082ad290adc5dd3a0c5
dusty <d.argyle@sarcos.com>
2018-10-20 15:04:02 -0600

    Adding solution to median of two sorted arrays

--- cpp/src/MedianOfTwoSortedArrays.cpp
+++ cpp/src/MedianOfTwoSortedArrays.cpp
@@ -15,6 +15,37 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         double median = 0.0;
+        int size = nums1.size() + nums2.size();
+        int middle = (size / 2);
+        int nums3[size];
+
+        int i1 = 0, i2 = 0;
+        int i3 = 0;
+        while (i1 < nums1.size() && i2 < nums2.size())
+        {
+            if (i1 > middle + 1 && i2 > middle + 1)
+                break;
+
+            if (nums1[i1] < nums2[i2])
+                nums3[i3++] = nums1[i1++];
+            else
+                nums3[i3++] = nums2[i2++];
+        }
+
+        while (i1 < nums1.size() && i1 < middle + 1)
+            nums3[i3++] = nums1[i1++];
+
+        while (i2 < nums2.size() && i2 < middle + 1)
+            nums3[i3++] = nums2[i2++];
+
+        if (size % 2 == 0)
+        {
+            median = (nums3[middle - 1] + nums3[middle]) / 2.0;
+        }
+        else
+        {
+            median = nums3[middle];
+        }
 
         return median;
     }
@@ -24,6 +55,7 @@ void test_example(vector<int>& nums1, vector<int>& nums2, double solution)
 {
     Solution solver;
     auto ans = solver.findMedianSortedArrays(nums1, nums2);
+
     assert(ans == solution);
 
     std::cout << "Passed: " << solution << std::endl;
@@ -46,6 +78,14 @@ int main(int argc, char** argv)
     v2.push_back(3);
     v2.push_back(4);
     test_example(v1, v2, 2.5);
+
+    v1.clear();
+    v2.clear();
+
+    v2.push_back(2);
+    v2.push_back(3);
+    test_example(v1, v2, 2.5);
+
 
     return 0;
 }
