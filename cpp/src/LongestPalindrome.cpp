ce40cb96faf524802d19d352b0818ede2665495b
dusty <d.argyle@sarcos.com>
2018-10-24 20:16:27 -0600

    Adding LongestPalindrome solution

--- cpp/src/LongestPalindrome.cpp
+++ cpp/src/LongestPalindrome.cpp
@@ -7,16 +7,42 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string longest_pal = "";
+        if (s.length() < 1)
+            return "";
 
-        return longest_pal;
+        int l = 0, r = 0;
+        for (int i = 0; i < s.length(); ++i)
+        {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > r - l) {
+                l = i - (len - 1) / 2;
+                r = i + len / 2;
+            }
+        }
+
+        return s.substr(l, r + 1 - l);
+    }
+
+    int expand(string s, int l, int r)
+    {
+        while (r < s.length() && l >= 0)
+        {
+            if (s[l] != s[r])
+                break;
+
+            --l;
+            ++r;
+        }
+        return r - l - 1;
     }
 };
 
@@ -34,10 +60,11 @@ void test_example(string s, vector<string> possible_answers)
             break;
         }
     }
+    std::cout << "Answer: " << ans << std::endl;
 
     assert(found == true);
 
-    std::cout << "Passed: " << ans << std::endl;
+    std::cout << "-> Passed" << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -47,6 +74,15 @@ int main(int argc, char** argv)
 
     // Example 2:
     test_example("cbbd", vector<string> {"bb"});
+
+    test_example("ac", vector<string> {"a", "c"});
+
+    // test_example("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg", )
+
+    Solution s;
+    string f = s.longestPalindrome("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg");
+
+    std::cout << f << std::endl;
 
     return 0;
 }
